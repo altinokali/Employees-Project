cd40ca9f5114e3851f1eebdc867456fd9f452f4f
altinokali <ali.altinok1998@gmail.com>
2022-02-24 00:56:01 +0300

    Request Class

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
-console.log("Test");
-console.log("Test2");
+import { Request } from "./request"
+// Selecet Elements
+
+
+const form = document.querySelector("#empoyee-form")
+const nameImput = document.querySelector("#name")
+const departmentImput = document.querySelector("#department")
+const salaryImput = document.querySelector("#salary")
+const employeeList = document.querySelector("#employees")
+const updateEmoployeeButton = document.querySelector("#update")
+
+const request = new Request("http://localhost:3000/employees")
+
+
+// request.get()
+// .then(employees => console.log(employees))
+// .catch((err) => console.log(err))
+
+// request.post({
+//     name : "Caner Altindag", department: "Civil Engineering", salary : 6200
+// }).then(employee => console.log(employee))
+// .catch((err) => console.log(err))
+
+
+// request.put(2,{
+//     name : "Aslı Altindag", department: "Civil Engineering", salary : 6200
+// }).then(employee => console.log(employee))
+// .catch((err) => console.log(err))
+
+request.delete(4)
+    .then(message => console.log(message))
+    .catch((err) => console.log(err))
