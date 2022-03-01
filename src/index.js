204ccb1876efa0446b8aa47d587e5a480e0660cb
altinokali <ali.altinok1998@gmail.com>
2022-03-01 21:06:41 +0300

    Insert and listing employees

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import { Request } from "./request"
+import { UI } from "./ui"
 // Selecet Elements
 
-
-const form = document.querySelector("#empoyee-form")
-const nameImput = document.querySelector("#name")
-const departmentImput = document.querySelector("#department")
-const salaryImput = document.querySelector("#salary")
-const employeeList = document.querySelector("#employees")
+const form = document.querySelector("#employee-form")
+const nameInput = document.querySelector("#name")
+const departmentInput = document.querySelector("#department")
+const salaryInput = document.querySelector("#salary")
+const employeesList = document.querySelector("#employees")
 const updateEmoployeeButton = document.querySelector("#update")
 
 const request = new Request("http://localhost:3000/employees")
 
+const ui = new UI()
+
+eventListeners();
+
+function eventListeners() {
+    document.addEventListener("DOMContentLoaded", getAllEmployees);
+    form.addEventListener("submit" , addEmployee);
+}
+
+function getAllEmployees() {
+        request.get()
+        .then(employees => {
+            ui.addAllEmployeesToUI(employees)
+        }).catch((err) => console.log(err))
+        
+}
 
 // request.get()
 // .then(employees => console.log(employees))
@@ -27,6 +43,27 @@ const request = new Request("http://localhost:3000/employees")
 // }).then(employee => console.log(employee))
 // .catch((err) => console.log(err))
 
-request.delete(4)
-    .then(message => console.log(message))
-    .catch((err) => console.log(err))
+// request.delete(4)
+//     .then(message => console.log(message))
+//     .catch((err) => console.log(err))
+
+function addEmployee(e) {
+  
+    const employeeName =nameInput.value.trim();
+    const employeeDepartment = departmentInput.value.trim();
+    const employeeSalary = salaryInput.value.trim();
+    
+    if(employeeName === "" || employeeDepartment === "" || employeeSalary === ""){
+        alert('Lütfen ilgili alanlari doldurunuz');
+    }
+    else
+    {
+    request.post({name: employeeName, department: employeeDepartment, salary: employeeSalary})
+    .then(employee => {ui.addEmployeeToUI(employee)})
+    .catch(err => console.log(err));
+    }
+   
+
+    ui.clearInputs()
+    e.preventDefault();
+}
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,47 @@
-class UI {
+export class UI{
     
+    constructor(){
+        this.employeesList = document.getElementById('employees');
+        this.updateButton = document.getElementById('update');
+        this.nameInput = document.getElementById('name');
+        this.departmentInput = document.getElementById('department');
+        this.salaryInput = document.getElementById('salary');
+    }
+ 
+    addAllEmployeesToUI(employees){
+ 
+        let result ="";
+        employees.forEach(employee => {
+            result +=`<tr>
+                        <td>${employee.name}</td>
+                        <td>${employee.department}</td>
+                        <td>${employee.salary}</td>
+                        <td>${employee.id}</td>
+                        <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+                        <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+                    </tr>`;
+        });
+       
+        console.log(this.employeesList);         
+        this.employeesList.innerHTML  = result; 
+    }
+    clearInputs() {
+        this.nameInput.value = "";
+        this.departmentInput.value= "";
+        this.salaryInput.value= "";
+
+    }
+
+    addEmployeeToUI(employee) {
+        this.employeesList.innerHTML += 
+        `<tr>
+            <td>${employee.name}</td>
+            <td>${employee.department}</td>
+            <td>${employee.salary}</td>
+            <td>${employee.id}</td>
+            <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+            <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+        </tr>`;
+    }
+ 
 }
